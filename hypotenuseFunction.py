eebfacfe128ac6f11b9604bc36e7289d367521ff
MinionGames <109187184+MinionGames@users.noreply.github.com>
2025-03-15 13:32:01 -0400

    Added Comments to Hypotenuse Function

--- hypotenuseFunction.py
+++ hypotenuseFunction.py
@@ -1,12 +1,17 @@
+# ----VARIABLES----
 leg1 = [3, 5, 7, 9]
 leg2 = [4, 12, 24, 40]
 
+# ----METHODS----
+# Hypotenuse Function
+# PRECONDITION: inputs are same length
 def getHypotenuse(leg1, leg2):
-	output = []
-	for i in range(len(leg1)):
-		output.append((leg1[i]**2 + leg2[i]**2)**0.5)
+	output = [] # init output array
+	for i in range(len(leg1)): # iterate through each value
+		output.append((leg1[i]**2 + leg2[i]**2)**0.5) # calculate for hypotenuse and add to output list
 	return output
 
-hypos = getHypotenuse(leg1, leg2)
-for i in hypos:
-	print(i)
+# -----RUNTIME----
+hypos = getHypotenuse(leg1, leg2) # calculate list of hypotenuses
+for i in hypos: # iterate through each value in list
+	print(i) # print said value
