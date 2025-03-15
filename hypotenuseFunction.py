9395bf8e64ec5a377b0af9cdac15a38f3ba0b22f
MinionGames <109187184+MinionGames@users.noreply.github.com>
2025-03-15 13:28:08 -0400

    Updated Hypotenuse Function

--- hypotenuseFunction.py
+++ hypotenuseFunction.py
@@ -1,7 +1,12 @@
-leg1 = 3
-leg2 = 4
+leg1 = [3, 5, 7, 9]
+leg2 = [4, 12, 24, 40]
 
 def getHypotenuse(leg1, leg2):
-	return (leg1**2 + leg2**2)**0.5
+	output = []
+	for i in range(len(leg1)):
+		output.append((leg1[i]**2 + leg2[i]**2)**0.5)
+	return output
 
-print(getHypotenuse(leg1, leg2))
+hypos = getHypotenuse(leg1, leg2)
+for i in hypos:
+	print(i)
